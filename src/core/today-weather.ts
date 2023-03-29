4d599f6c0125d1ecec4829724f86159bd5a12ef2
prototype9449 <7286647+prototype9449@users.noreply.github.com>
2023-03-30 00:29:09 +0300

    add static mock

--- src/core/today-weather.ts
+++ src/core/today-weather.ts
@@ -1,5 +1,21 @@
+import { registerMock } from "@exness-tech/mock-xhr-request";
 import { axiosInstance } from "./axiosInstance";
 import { TodayWeather, TrailPlace } from "../../gateway/src/types";
+
+const missingWeatherData: TodayWeather[] = [
+  {
+    forecast: "clear-day",
+    place: "Paphos",
+    temperature: 15,
+    description: "Phew, quite windy",
+    wind: 51,
+    humidity: 40,
+    feelsLike: 22,
+  },
+];
+
+registerMock("/api/weather/today:?search", "get", 200, missingWeatherData)
+  .withName("windyDayInPaphos");
 
 export const getTodayWeather = async (places: TrailPlace[]): Promise<TodayWeather[]> => {
   const queryParams = `places=${places.join(",")}`;
