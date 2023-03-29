bad2141dc1cb53629ebf86eda5643f3bf1fa3985
prototype9449 <7286647+prototype9449@users.noreply.github.com>
2023-03-29 22:23:12 +0300

    add wrapAxiosAdapter

--- src/core/axiosInstance.ts
+++ src/core/axiosInstance.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
+import { wrapAxiosAdapter } from "@exness-tech/mock-xhr-request";
 
 const axiosInstance = axios.create();
+wrapAxiosAdapter(axiosInstance);
 
 export { axiosInstance };
